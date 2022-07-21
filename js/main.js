313cbe69ff99d5a7bc6404b9fae118c5c8548334
waldoozki <axelozv04@gmail.com>
2022-07-20 19:36:33 -0600

    RoomHomePage (responsive)

--- js/main.js
+++ js/main.js
@@ -26,31 +26,3 @@
 
   
 })();
-
-// sliderMovil
-(function(){
-  const sliderMovil = [...document.querySelectorAll('.main__room--movil')];
-  const buttonAfter = document.querySelector('#after');
-  const buttonBefore = document.querySelector('#before');
-  console.log(sliderMovil)
-  let value;
-
-  buttonAfter.addEventListener('click', ()=>{
-    changePosition(1);
-  });
-  buttonBefore.addEventListener('click', ()=>{
-    changePosition(-1);
-  });
-
-  const changePosition = (add)=>{
-    const currentElement = document.querySelector('.main__room--show').dataset.id;
-    value = Number(currentElement);
-    value += add;
-
-    sliderMovil[Number(currentElement)-1].classList.remove('main__room--show');
-    if(value === sliderMovil.length+1 || value === 0){
-      value = value === 0 ? sliderMovil.length :1;
-    }
-    sliderMovil[value-1].classList.add('main__room--show');
-  }
-})();
